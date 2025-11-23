2ca734f9137af32da4993beae17f07add4c2e292
Norbert Toth <79476468+nt5664@users.noreply.github.com>
2025-11-23 21:13:14 +0100

    Opponent's progress is now shown in lockout mode

---
+++ HPBingoCounter/HPBingoCounter/Coverters/EnumToVisibilityConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace HPBingoCounter.Coverters
+{
+    internal class EnumToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        #region MarkupExtension overrides
+        static EnumToVisibilityConverter()
+        {
+            Instance = new EnumToVisibilityConverter();
+        }
+
+        public static EnumToVisibilityConverter Instance { get; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Instance;
+        }
+        #endregion
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null || value is not Enum || parameter is null)
+                throw new ArgumentException("Both 'value' and 'parameter' cannot be null and 'value' must be an enum value");
+
+            object? enumValue;
+            if (!Enum.TryParse(value.GetType(), parameter.ToString(), out enumValue))
+                return Visibility.Collapsed;
+
+            return value.Equals(enumValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
